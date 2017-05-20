375c8917045a0efff08a92937a38b2948e7e781b
lgarcia <lleirgarcia@gmail.com>
2017-05-20 13:31:03 +0200

    selenium webdriver

--- src/test/java/com/practicaivan/app/SeleniumTestCases1.java
+++ src/test/java/com/practicaivan/app/SeleniumTestCases1.java
@@ -1,15 +1,34 @@
 package com.practicaivan.app;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 /**
  * Created by lleir on 20/5/17.
  */
 public class SeleniumTestCases1 {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("http://www.google.es");
+//
+//        System.out.println("Google abierto con exito. Selenium Test Case!");
+//
+//        try {
+//            Thread.sleep(5);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        driver.quit();
+//    }
 
+    @Test
+    public void a() throws Exception {
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://www.google.es");
